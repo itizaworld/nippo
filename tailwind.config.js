bf2346b3f25026e136ea499810efff770b6396f3
itizawa <itizawa.pen@gmail.com>
2023-10-29 11:14:05 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-undef */
+import { nextui } from '@nextui-org/react';
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [
+    nextui({
+      layout: {},
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: '#6366f1',
+              foreground: '#fff',
+            },
+          },
+        },
+        dark: {},
+      },
+    }),
+  ],
+}
+export default config
