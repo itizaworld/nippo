0bee1b42e480ad933c7dbf70523a5255b51d2cf9
itizawa <itizawa.pen@gmail.com>
2023-10-30 23:46:08 +0900

    スタイルの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default async function Page() {
   return (
-    <div className="bg-white drop-shadow-sm">
+    <div className="drop-shadow-sm">
       <div className="min-h-[500px] max-w-[1024px] mx-auto flex flex-col-reverse md:flex-row justify-center gap-[16px] md:gap-[48px]">
         <div className="flex flex-col justify-center px-[16px] pb-[32px]">
-          <h1 className="text-2xl font-bold mb-[8px]">あなたの切り口で記事をまとめよう</h1>
-          <p className="text-slate-600 mb-[4px]">オンライン日報はURLをまとめたページを気軽に作れるアプリケーションです。</p>
-          <p className="text-slate-600 mb-[24px]">情報の整理や共有にお使いください。</p>
+          <h1 className="text-2xl font-bold mb-[8px]">目標達成に向けて一歩を踏み出そう</h1>
+          <p className="text-slate-600 mb-[24px]">オンライン日報はURLをまとめたページを気軽に作れるアプリケーションです。</p>
+          <p className="text-slate-600">Coming Soon...</p>
         </div>
       </div>
     </div>
