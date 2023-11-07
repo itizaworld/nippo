9cb93d414637724e06c4b8952de7dfca8a1c8ff1
itizawa <itizawa.pen@gmail.com>
2023-11-07 22:36:32 +0900

    Add front-end URLs for terms and policy

--- src/app/_constants/urls.ts
+++ src/app/_constants/urls.ts
@@ -1,6 +1,9 @@
+const frontUrl = process.env.NEXT_PUBLIC_FRONT_URL;
 const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 export const URLS = {
   TOP: '/',
   NOTE_NEW: '/notes/new',
+  TERM: `${frontUrl}/terms`,
+  POLICY: `${frontUrl}/policy`,
   LOGIN_TO_BACKEND: `${serverUrl}/auth/google`,
 };
