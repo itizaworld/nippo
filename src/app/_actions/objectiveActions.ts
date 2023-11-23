84b1bb14f9fab7357945df944cfdcfac2e4db892
itizawa <itizawa.pen@gmail.com>
2023-11-23 22:49:49 +0900

    Objective 作成後詳細ページにリダイレクトするようにした

--- src/app/_actions/objectiveActions.ts
+++ src/app/_actions/objectiveActions.ts
@@ -1,13 +1,19 @@
 'use server';
 
+import { redirect } from 'next/navigation';
 import { API_OBJECTIVE } from '../_constants/apiUrls';
+import { URLS } from '../_constants/urls';
+import { Objective } from '~/domains/Objective';
 import { apiPost } from '~/libs/apiClient';
 
 export const postObjective = async (name: string) => {
-  return await apiPost(API_OBJECTIVE(), {
+  return await apiPost<{ object: Objective }>(API_OBJECTIVE(), {
     body: JSON.stringify({
       name,
       description: name,
     }),
+  }).then((res) => {
+    const { _id } = res.object;
+    redirect(URLS.OBJECTIVE(_id));
   });
 };
--- src/app/_constants/urls.ts
+++ src/app/_constants/urls.ts
@@ -3,6 +3,7 @@ const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 export const URLS = {
   TOP: '/',
   NOTE_NEW: '/notes/new',
+  OBJECTIVE: (id: string) => `/objectives/${id}`,
   TERM: `${frontUrl}/terms`,
   POLICY: `${frontUrl}/policy`,
   LOGIN_TO_BACKEND: `${serverUrl}/auth/google`,
