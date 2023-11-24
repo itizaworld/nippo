3f0166a04c843b5af6a7e4c40f74746d3f0569e1
itizawa <itizawa.pen@gmail.com>
2023-11-24 22:28:26 +0900

    getObjective を実装した

--- src/app/_actions/objectiveActions.ts
+++ src/app/_actions/objectiveActions.ts
@@ -1,10 +1,14 @@
 'use server';
 
 import { redirect } from 'next/navigation';
-import { API_OBJECTIVE } from '../_constants/apiUrls';
+import { API_OBJECTIVE, API_OBJECTIVE_ID } from '../_constants/apiUrls';
 import { URLS } from '../_constants/urls';
 import { Objective } from '~/domains/Objective';
-import { apiPost } from '~/libs/apiClient';
+import { apiGet, apiPost } from '~/libs/apiClient';
+
+export const getObjective = async (_id: string) => {
+  return await apiGet<{ object: Objective }>(API_OBJECTIVE_ID(_id), {});
+};
 
 export const postObjective = async (name: string) => {
   return await apiPost<{ object: Objective }>(API_OBJECTIVE(), {
--- src/app/_constants/apiUrls.ts
+++ src/app/_constants/apiUrls.ts
@@ -2,3 +2,4 @@
 export const API_ME = () => `/api/me`;
 
 export const API_OBJECTIVE = () => `/api/objectives`;
+export const API_OBJECTIVE_ID = (_id: string) => `/api/objectives/${_id}`;
