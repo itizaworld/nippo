0df6831a687d768054508d9f5e9a7973d1b97898
itizawa <itizawa.pen@gmail.com>
2023-11-25 22:46:31 +0900

    Add API endpoint for objective nippo

--- src/app/_constants/apiUrls.ts
+++ src/app/_constants/apiUrls.ts
@@ -4,3 +4,5 @@ export const API_ME = () => `/api/me`;
 export const API_OBJECTIVE = () => `/api/objectives`;
 export const API_OBJECTIVE_ID = (_id: string) => `/api/objectives/${_id}`;
 export const API_OBJECTIVE_ME = () => `/api/objectives/me`;
+
+export const API_OBJECTIVE_ID_NIPPO = (_id: string) => `/api/objectives/${_id}/nippos`;
