7e8d80299f8eb7a06b576a09412fd0bba0038339
itizawa <itizawa.pen@gmail.com>
2023-11-25 22:11:33 +0900

    fix ssr

--- src/app/objectives/[id]/_components/Editor/NippoEditor.tsx
+++ src/app/objectives/[id]/_components/Editor/NippoEditor.tsx
@@ -1,14 +1,18 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { useEffect } from 'react';
 
 const MarkdownEditor = dynamic(() => import('@uiw/react-markdown-editor').then((mod) => mod.default), { ssr: false });
 
 const mdStr = `# This is a H1  \n## This is a H2  \n###### This is a H6`;
 
 export const NippoEditor = () => {
-  // NOTE: 初期はlightモード固定にする。アプリ自体がダークモードに対応したらブラウザの設定に合わせる
-  document.documentElement.setAttribute('data-color-mode', 'light');
+  useEffect(() => {
+    if (!document.documentElement) return;
+    // NOTE: 初期はlightモード固定にする。アプリ自体がダークモードに対応したらブラウザの設定に合わせる
+    document.documentElement.setAttribute('data-color-mode', 'light');
+  }, []);
 
   return (
     <MarkdownEditor
