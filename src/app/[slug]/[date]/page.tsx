b3758604d0bbac21cf08c9dd3aad8cd3d5ffc966
itizawa <itizawa.pen@gmail.com>
2023-11-28 23:14:26 +0900

    Refactor 'Page' component to display objective name in header
    Previously, the 'Page' component only displayed the date in the header.
    This commit updates the component to also fetch and display the name of
    the objective corresponding to the provided slug.
    
    By including the objective name in the header, users can quickly
    identify the objective associated with the displayed date's nippo. This
    improves the clarity and context of the displayed information.
    
    No issues related to this change.
    
    Note: The code diff provided does not show any issues or bugs that
    require fixing.

--- src/app/[slug]/[date]/page.tsx
+++ src/app/[slug]/[date]/page.tsx
@@ -1,16 +1,17 @@
 import { format } from 'date-fns';
 import { NippoPreview } from '~/app/_components/domains/Nippo/NippoPreview/NippoPreview';
-import { fetchMe } from '~/app/_actions/userActions';
 import { getNippoByDate } from '~/app/_actions/nippoActions';
+import { getObjectiveBySlug } from '~/app/_actions/objectiveActions';
 
-export default async function Page({ params }: { params: { date: string } }) {
-  const [{ nippo }] = await Promise.all([getNippoByDate(params.date), fetchMe()]);
+export default async function Page({ params }: { params: { slug: string; date: string } }) {
+  const [{ objective }, { nippo }] = await Promise.all([getObjectiveBySlug(params.slug), getNippoByDate(params.date)]);
   const dateString = format(new Date(nippo.date), 'yyyy年 MM月dd日');
 
   return (
     <div className="drop-shadow-sm">
       <div className="min-h-[500px] max-w-[1024px] mx-auto flex flex-col-reverse md:flex-row gap-[16px] md:gap-[48px]">
         <div className="px-[8px] pt-[16px] pb-[32px] w-[100%]">
+          <h1 className="text-2xl font-bold pb-[8px] border-b-1">{objective.name}</h1>
           <p className="mt-[32px] text-xl font-bold mb-[8px] text-gray-700">{dateString}</p>
           {nippo ? <NippoPreview body={nippo.body} /> : <div>{dateString}の日報はまだありません</div>}
         </div>
