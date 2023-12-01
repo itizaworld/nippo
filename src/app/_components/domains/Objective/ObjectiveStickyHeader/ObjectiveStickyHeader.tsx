13c03c7ae4229bf032671994bf9124318290aaf3
itizawa <itizawa.pen@gmail.com>
2023-12-01 22:29:44 +0900

    ObjectiveStickyHeaderにリンクを設定した

--- src/app/_components/domains/Objective/ObjectiveStickyHeader/ObjectiveStickyHeader.tsx
+++ src/app/_components/domains/Objective/ObjectiveStickyHeader/ObjectiveStickyHeader.tsx
@@ -1,4 +1,6 @@
+import { Link } from '@nextui-org/link';
 import { FC } from 'react';
+import { URLS } from '~/app/_constants/urls';
 import { Objective } from '~/domains/Objective';
 
 type Props = {
@@ -7,7 +9,9 @@ type Props = {
 export const ObjectiveStickyHeader: FC<Props> = ({ objective }) => {
   return (
     <h1 className="text-xl md:text-2xl font-bold py-[8px] border-b-1 sticky md:static mx-[-8px] px-[8px] md:mx-[0px] md:px-[0px] top-0 z-10 bg-slate-50">
-      {objective.name}
+      <Link href={URLS.SLUG(objective.slug)} className="cursor-pointer text-inherit">
+        {objective.name}
+      </Link>
     </h1>
   );
 };
