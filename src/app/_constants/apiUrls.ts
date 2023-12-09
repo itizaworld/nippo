c89539dad50e3f2e4ffa6ffaa1fec656c33a6d11
itizawa <itizawa.pen@gmail.com>
2023-12-09 10:23:17 +0900

    api の修正

--- src/app/_constants/apiUrls.ts
+++ src/app/_constants/apiUrls.ts
@@ -6,5 +6,5 @@ export const API_OBJECTIVE_ID = (_id: string) => `/api/objectives/${_id}`;
 export const API_OBJECTIVE_SLUG = (slug: string) => `/api/objectives/slug/${slug}`;
 export const API_OBJECTIVE_ME = () => `/api/objectives/me`;
 
-export const API_OBJECTIVE_ID_NIPPO = (_id: string) => `/api/objectives/${_id}/nippos`;
+export const API_OBJECTIVE_ID_NIPPO = (_id: string, page: number) => `/api/objectives/${_id}/nippos?page=${page}`;
 export const API_NIPPO_BY_DATE = (slug: string, data: string) => `/api/nippos/by-date?date=${data}&slug=${slug}`;
