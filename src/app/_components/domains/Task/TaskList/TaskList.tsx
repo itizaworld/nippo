671a07a4dc20a482e5e556e17299ccb621ce2189
itizawa <itizawa.pen@gmail.com>
2023-12-22 00:36:05 +0900

    add gap

--- src/app/_components/domains/Task/TaskList/TaskList.tsx
+++ src/app/_components/domains/Task/TaskList/TaskList.tsx
@@ -17,13 +17,15 @@ export const TaskList: FC<Props> = ({ objectiveId, tasks }) => {
   return (
     <div className="w-full">
       <p className="text-md font-bold mb-[16px] text-gray-700">タスク一覧</p>
-      {tasks.map((task) => {
-        return (
-          <Card key={task._id} className="md:p-[8px] p-[4px] flex flex-col rounded-[8px]" shadow="none" radius="none">
-            ここにタスク一覧が表示されます
-          </Card>
-        );
-      })}
+      <div className="flex flex-col gap-[8px]">
+        {tasks.map((task) => {
+          return (
+            <Card key={task._id} className="md:p-[8px] p-[4px] flex flex-col rounded-[8px] cursor-pointer" shadow="none" radius="none">
+              ここにタスク一覧が表示されます
+            </Card>
+          );
+        })}
+      </div>
       <Button className="mt-[8px]" size="sm" color="primary" variant="light" startContent={<Icon icon="PLUS" />} onClick={() => onOpen()}>
         タスクの追加
       </Button>
